52ed2ae69c796aa5a6922c28a39533835a219096
amirhilal044 <amiramirhilal@gmail.com>
2024-11-08 22:33:04 +0200

    added slider for video player screen

--- lib/screens/video_player_screen.dart
+++ lib/screens/video_player_screen.dart
@@ -1,5 +1,3 @@
-//video_player_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -14,6 +12,7 @@ class VideoPlayerScreen extends StatefulWidget {
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
+  bool _isControlsVisible = true; // To show/hide controls on tap
 
   @override
   void initState() {
@@ -22,12 +21,27 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       ..initialize().then((_) {
         setState(() {}); // Update the UI when the video is loaded
       });
+    _controller.addListener(() {
+      setState(() {}); // Update the UI when video plays or pauses
+    });
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  void _togglePlayPause() {
+    setState(() {
+      _controller.value.isPlaying ? _controller.pause() : _controller.play();
+    });
+  }
+
+  void _toggleControls() {
+    setState(() {
+      _isControlsVisible = !_isControlsVisible;
+    });
   }
 
   @override
@@ -38,24 +52,91 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       ),
       body: Center(
         child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
+            ? GestureDetector(
+                onTap: _toggleControls, // Toggle controls on tap
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    ),
+                    if (_isControlsVisible) _buildControls(), // Show controls
+                  ],
+                ),
               )
             : const CircularProgressIndicator(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ),
     );
+  }
+
+  Widget _buildControls() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(
+                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                  color: Colors.white,
+                ),
+                onPressed: _togglePlayPause,
+              ),
+              Text(
+                _formatDuration(_controller.value.position),
+                style: const TextStyle(color: Colors.white),
+              ),
+              Expanded(
+                child: SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    activeTrackColor: const Color.fromARGB(
+                        255, 234, 242, 255), // Color of the active slider
+                    inactiveTrackColor:
+                        Colors.grey.withOpacity(0.5), // Inactive slider color
+                    thumbColor: Colors.blue, // Color of the thumb handle
+                    overlayColor: Colors.blue
+                        .withOpacity(0.2), // Overlay color when dragging
+                    thumbShape:
+                        const RoundSliderThumbShape(enabledThumbRadius: 8.0),
+                    overlayShape:
+                        const RoundSliderOverlayShape(overlayRadius: 16.0),
+                  ),
+                  child: Slider(
+                    value: _controller.value.position.inSeconds.toDouble(),
+                    min: 0,
+                    max: _controller.value.duration.inSeconds.toDouble(),
+                    onChanged: (value) {
+                      setState(() {
+                        _controller.seekTo(Duration(seconds: value.toInt()));
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Text(
+                _formatDuration(_controller.value.duration),
+                style: const TextStyle(color: Colors.white),
+              ),
+              IconButton(
+                icon: const Icon(Icons.fullscreen, color: Colors.white),
+                onPressed: () {
+                  // Fullscreen functionality here (if needed)
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  String _formatDuration(Duration position) {
+    final minutes = position.inMinutes;
+    final seconds = position.inSeconds % 60;
+    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
 }
