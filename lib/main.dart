fcbb1a1cfafe746f3232391a84a4d72e099482b0
amirhilal044 <amiramirhilal@gmail.com>
2024-12-06 22:02:13 +0200

    restrict to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:shootsolo/providers/video_modal_provider.dart';
 import 'providers/video_provider.dart';
 import 'app.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(
     MultiProvider(
       providers: [
