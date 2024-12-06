8c1beff570ff31ff3b22fbf58dc9c8c1da3cb67e
amirhilal044 <amiramirhilal@gmail.com>
2024-12-06 13:18:49 +0200

    updated app logo

--- lib/app.dart
+++ lib/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Video App',
+      title: 'Shootsolo',
       theme: ThemeData.dark(),
       initialRoute: '/',
       routes: {
