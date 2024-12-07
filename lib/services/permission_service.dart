2e7f34cf8e30111e9ba256375ceda7fdef5dfd16
Amir Hilal <amiramirhilal@gmail.com>
2024-12-07 19:01:50 +0200

    added requesting permission for ios devices <> updated configuration settings for ios

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -1,31 +1,53 @@
+import 'dart:io';
+
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionService {
   static Future<bool> requestPermissions() async {
-    AndroidDeviceInfo build = await DeviceInfoPlugin().androidInfo;
+    if (!Platform.isIOS && !Platform.isAndroid) {
+      // If on a non-mobile platform (e.g., simulator), skip permissions.
+      return true;
+    }
 
+    // Request camera permission
     final cameraStatus = await Permission.camera.request();
+    if (cameraStatus.isDenied || cameraStatus.isPermanentlyDenied) {
+      openAppSettings();
+      return false;
+    }
+
+    // Request microphone permission
     final microphoneStatus = await Permission.microphone.request();
+    if (microphoneStatus.isDenied || microphoneStatus.isPermanentlyDenied) {
+      openAppSettings();
+      return false;
+    }
 
-    bool storagePermissionGranted;
-
-    if (build.version.sdkInt >= 33) {
-      // Android 13+
-      final videoPermissionStatus = await Permission.videos.request();
-      storagePermissionGranted = videoPermissionStatus.isGranted;
-    } else if (build.version.sdkInt <= 29) {
-      var storageStatus = await Permission.storage.request();
-      storagePermissionGranted = storageStatus.isGranted;
-    } else {
-      storagePermissionGranted = true;
+    // Request photo library permission
+    final photoStatus = await Permission.photos.request();
+    if (photoStatus.isDenied || photoStatus.isPermanentlyDenied) {
+      openAppSettings();
+      return false;
     }
 
     return cameraStatus.isGranted &&
         microphoneStatus.isGranted &&
-        storagePermissionGranted;
+        photoStatus.isGranted;
   }
 
+  static Future<bool> _requestAndroidSpecificPermissions() async {
+    // Check Android SDK version
+    final videoPermissionStatus = await Permission.videos.status;
+    if (!videoPermissionStatus.isGranted) {
+      final videoPermissionRequest = await Permission.videos.request();
+      if (!videoPermissionRequest.isGranted && videoPermissionRequest.isPermanentlyDenied) {
+        openAppSettings();
+        return false;
+      }
+    }
+    return true;
+  }
   static Future<bool> isStoragePermissionGranted() async {
     AndroidDeviceInfo build = await DeviceInfoPlugin().androidInfo;
     if (build.version.sdkInt >= 33) {
